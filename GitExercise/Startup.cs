2d6cdece02e70bbccba4e54a8211957e43728d01
Iv40 <ivcho465@gmail.com>
2022-11-16 08:41:40 +0200

    startup edit

--- GitExercise/Startup.cs
+++ GitExercise/Startup.cs
@@ -44,6 +44,15 @@ namespace GitExercise
                 case "m":
                     OptionsManager.Multiply(a, b);
                     break;
+                case "pow":
+                    OptionsManager.Power(a, b);
+                    break;
+                case "log":
+                    OptionsManager.Log(a, b);
+                    break;
+                case "fact":
+                    OptionsManager.Factorial(a, b);
+                    break;
             }
 
             Console.WriteLine("Pres any key to close the app...");
