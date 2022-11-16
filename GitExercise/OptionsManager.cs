b01a5294443ce50532c46bb98d2bf6cf54f848a1
Iv40 <ivcho465@gmail.com>
2022-11-16 08:54:56 +0200

    edit optionmanager

--- GitExercise/OptionsManager.cs
+++ GitExercise/OptionsManager.cs
@@ -10,7 +10,10 @@ namespace GitExercise
             "m - Multiply",
             "d - Divide",
             "sabs - Subtract Abs"
-        };
+            "pow - Power"
+            "log - Logarithm"
+            "fact - Sum of 2 factorials"
+        };      
         public static void Divide(double a, double b)
         {
             Console.WriteLine($"| {a} : {b} = {a / b}");
@@ -28,6 +31,29 @@ namespace GitExercise
         public static void SubtractAbs(double a, double b)
         {
             Console.WriteLine($"{a} - {b} = {Math.Abs(a - b)}");
+        }
+        public static void Power(double a, double b)
+        {
+            Console.WriteLine($"{a} ^ {b} = {Math.Pow(a, b)}");
+        }
+        public static void Log(double a, double b)
+        {
+            Console.WriteLine($"Log of {a} with base {b} = {Math.Log(a, b)}");
+        }
+        public static void Factorial(double a, double b)
+        {
+            long factA = CalculateFact((int)a);
+            long factB = CalculateFact((int)b);
+            Console.WriteLine($"{a}! + {b}! = {(int)a}! + {(int)b}! = {factA + factB}");
+        }
+        private static long CalculateFact(int a)
+        {
+            long result = a;
+            for (int i = a - 1; i >= 1 ; i--)
+            {
+                result = result * i;
+            }
+            return result;
         }
     }
 }
